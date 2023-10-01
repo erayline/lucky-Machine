1eeb6aa4d02a3f8b5ad5fe77c190d45dd9f2ca4f
erayline <144234072+erayline@users.noreply.github.com>
2023-10-01 20:17:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let hersey = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789012345677890";
+hersey = (hersey.split(''));
+
+
+
+
+function generate(){
+    arkaplanyazisi = " "
+    for (let i=0; i<6000; i++){
+        let rastgele = Math.random()
+        rastgele = rastgele*100
+        rastgele = rastgele.toFixed(0)
+        arkaplanyazisi += hersey[rastgele]
+    }
+    console.log(arkaplanyazisi)
+    return arkaplanyazisi
+}
+
+function arkplancevir(){
+    document.getElementById("arkaplanimiz").innerText = generate()
+}
+document.getElementById("arkaplanimiz").innerText = generate()
+
+
+
+document.addEventListener("mousemove", function(event) {
+    arkplancevir()
+});
+
+
+
+setInterval(arkplancevir,100)
