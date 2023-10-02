e28efe5ef3f57de73508895fde5cc10239ae63a9
erayline <144234072+erayline@users.noreply.github.com>
2023-10-02 22:23:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ hersey = (hersey.split(''));
 
 function generate(){
     arkaplanyazisi = " "
-    for (let i=0; i<6000; i++){
+    for (let i=0; i<10000; i++){
         let rastgele = Math.random()
         rastgele = rastgele*100
         rastgele = rastgele.toFixed(0)
         arkaplanyazisi += hersey[rastgele]
     }
-    console.log(arkaplanyazisi)
     return arkaplanyazisi
 }
 
-function arkplancevir(){
+function arkaplancevir(){
     document.getElementById("arkaplanimiz").innerText = generate()
 }
 document.getElementById("arkaplanimiz").innerText = generate()
@@ -24,9 +23,74 @@ document.getElementById("arkaplanimiz").innerText = generate()
 
 
 document.addEventListener("mousemove", function(event) {
-    arkplancevir()
+    arkaplancevir()
 });
 
+setInterval(arkaplancevir,1000)
 
 
-setInterval(arkplancevir,10000)
+let harita = [
+    "a",
+    "1",
+    "x",
+    "Z",
+    "{"
+]
+
+
+
+let ilkKolonCekimi = []
+let ikinciKolonCekimi = []
+let ucuncuKolonCekimi = []
+
+function cekimOlustur(cekim){
+    for(let i=0; i<100; i++){
+        let miniList = []
+        for(let i=0;i<3;i++){
+            rastgeleHarf = harita[Math.floor(Math.random()*5)]
+            miniList.push(rastgeleHarf)
+        }
+        cekim.push(miniList)
+    }
+    
+}
+
+
+
+function susluDegisimZamanla(kolonumuz,idimiz,satir){
+    for(let i=0; i<45;i++){
+        setTimeout(function() {
+            cekimOlustur(kolonumuz)
+            document.getElementById(idimiz).innerText = kolonumuz[i][satir]
+            console.log("en zeki benim")
+        }, i* 10 + i**2);
+        
+    }
+}
+
+function ilkKolonHazir(){
+    susluDegisimZamanla(ilkKolonCekimi,"11",0)
+    susluDegisimZamanla(ilkKolonCekimi,"22",1)
+    susluDegisimZamanla(ilkKolonCekimi,"33",2)
+}
+function ikinciKolonHazir(){
+    setTimeout(function(){
+        susluDegisimZamanla(ikinciKolonCekimi,"44",0)
+        susluDegisimZamanla(ikinciKolonCekimi,"55",1)
+        susluDegisimZamanla(ikinciKolonCekimi,"66",2)
+    },400)
+}
+function ucuncuKolonHazir(){
+    setTimeout(function(){
+        susluDegisimZamanla(ucuncuKolonCekimi,"77",0)
+        susluDegisimZamanla(ucuncuKolonCekimi,"88",1)
+        susluDegisimZamanla(ucuncuKolonCekimi,"99",2)
+    }, 800);
+}
+
+function cevir(){
+    ilkKolonHazir()
+    ikinciKolonHazir()
+    ucuncuKolonHazir()
+    console.log(ilkKolonCekimi)
+}
